ad146259c08b74f78766322360dba42e461de757
Emzzy241 <emzzyoluwole@gmail.com>
2023-12-03 17:34:49 +0100

    game completed

--- PingPong/Program.cs
+++ PingPong/Program.cs
@@ -7,37 +7,43 @@ namespace PingPong.Models
     {
         static void Main()
         {
-            Console.WriteLine("Enter a number");
+           while (true)
+           {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Hello My Beautiful User, Welcome to the Dynasty's PING PONG Tournament");
+                Console.WriteLine();
+                Console.WriteLine("Please Enter a Number greater than 0");
+                string num = Console.ReadLine();
+                int numInt = int.Parse(num);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Game Starts!");
 
-            string num = Console.ReadLine();
-            int numInt = int.Parse(num);
 
-            // MyPingPong.PrintPingPongSequence(numInt);
+                    for (int i = 1; i <= numInt; i++)
+                    {
+                        if (i % 5 == 0 && i % 3 == 0)
+                        {
+                            Console.WriteLine("Ping-Pong");
+                        }
+                        else if (i % 3 == 0)
+                        {
+                            Console.WriteLine("Ping");
+                        }
+                        else if (i % 5 == 0)
+                        {
+                            Console.WriteLine("Pong");
+                        }
+                        else
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Lets go Again");
+           }
 
-            // List<int> numList = new List<int>(){numInt};
-            
-            // static void PrintPingPongSequence()
-            // {
-                for (int i = 1; i <= numInt; i++)
-                {
-                    if (i % 5 == 0 && i % 3 == 0)
-                    {
-                        Console.WriteLine("Ping-Pong");
-                    }
-                    else if (i % 3 == 0)
-                    {
-                        Console.WriteLine("Ping");
-                    }
-                    else if (i % 5 == 0)
-                    {
-                        Console.WriteLine("Pong");
-                    }
-                    else
-                    {
-                        Console.WriteLine(i);
-                    }
-                }
-            // }
 
         }
     }
